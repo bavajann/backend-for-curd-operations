20f00bf2464768501155b04b801d578629521806
bavajann <bavajann9@gmail.com>
2024-09-20 01:19:05 +0530

    modified files

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Notes API');
 });
 
-// Error handling middleware (Place this before app.listen)
+// Error handling middleware 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
